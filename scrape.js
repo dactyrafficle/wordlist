84f8ef84dd96cb8d8a0e831c9086900a317fe8b8
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-04-22 22:17:40 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,23 +1,34 @@
 var words = [];
-var tagList = ['SPAN', 'LI', 'A', 'DIV'];
+var tagList = ['BODY', 'SPAN', 'UL', 'LI', 'A', 'DIV', 'H1', 'H2', 'H3', 'H4'];
 
 function abc(element) {
 
-  if (element.children.length > 0 && tagList.indexOf(element.tagName) !== -1) {
-    // if there are children elements, loop over them
-    for (var i = 0; i < element.children.length; i++) {
-      abc(element.children[i]);
-    }
-  } else {
+  if (tagList.indexOf(element.tagName) !== -1) {
+  
 
-    // if there are no children elements, grab the inner text of the element
-    let x = element.innerText.split(' ');
-    //console.log(element.tagName);
-    
-    for (var i = 0; i < x.length; i++) {
-      // add each word to the array
-      words.push(x[i]);
-    }  
+		if (element.children.length > 0) {
+		
+			// if there are children elements, loop over them
+			for (var i = 0; i < element.children.length; i++) {
+				abc(element.children[i]);
+			}
+			
+		} else {
+			   
+			//words.push(element.innerText);
+			
+			let x = element.innerText.split(' ');
+			
+
+			for (var i = 0; i < x.length; i++) {
+			
+				// add each word to the array
+				words.push(x[i]);
+			}  
+		}
+
+  } else {
+    // if tag isnt on the approved tag list, do nothing
   }
 
 }
